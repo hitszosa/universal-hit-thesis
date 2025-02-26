32300f1a93638b57f6ddf3b04fefd8ab29fc5827
Shiroki Satsuki <me@shirok1.dev>
2025-02-27 02:06:35 +0800

    ci: use Haixing-Hu/latex-chinese-fonts for fonts

--- ci/build.sh
+++ ci/build.sh
@@ -5,5 +5,5 @@ file_basenames=($(basename -s .typ templates/*.typ))
 mkdir -p build/
 
 for basename in "${file_basenames[@]}"; do
-    typst compile "templates/${basename}.typ" "build/${basename}.pdf" --root .
+    typst compile "templates/${basename}.typ" "build/${basename}.pdf" --root . --font-path fonts
 done
