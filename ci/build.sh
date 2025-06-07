af3a25396e2ce04b95509f1569800e690971ee31
Shiroki Satsuki <me@shirok1.dev>
2025-06-07 11:27:31 +0800

    ci: 删除遗留的 build.sh

--- ci/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-file_basenames=($(basename -s .typ templates/*.typ))
-
-mkdir -p build/
-
-for basename in "${file_basenames[@]}"; do
-    typst compile "templates/${basename}.typ" "build/${basename}.pdf" --root . --font-path fonts
-done
